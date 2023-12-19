d50e668a451a713510ac8572067849d63e0bda5d
Tejas M <97739480+playgms@users.noreply.github.com>
2023-12-19 19:33:57 +0530

    Update p6.tcl

--- p6.tcl
+++ p6.tcl
@@ -14,8 +14,8 @@ set val(rp) DSDV
 
 set ns [new Simulator]
 set tf [open ex6.tr w]
-$ns trace-all $tf
 set nf [open ex6.nam w]
+$ns trace-all $tf
 $ns namtrace-all-wireless $nf $val(x) $val(y)
 
 set prop [new $val(prop)]
@@ -32,40 +32,39 @@ $ns node-config -adhocRouting $val(rp) \
                 -ifqType $val(ifq) \
                 -llType $val(ll) \
                 -antType $val(ant) \
-                -ifqLen $val(ifqlen) \              
+                -ifqLen $val(ifqlen) \
                 -topoInstance $topo \
                 -agentTrace ON \
                 -routeTrace ON \
-                -macTrace ON 
-                
-for {set i 0} {$i < $val(nn)} {incr i} {
+                -macTarce ON
+
+for {set i 0} {$i < $val(nn) } {incr i} {
 set node_($i) [$ns node]
-$node_($i) random_motion 0
+$node_($i) random-motion 0
 }
-                
-for {set i 0} {$i < $val(nn)} {incr i} {
+
+for {set i 0} {$i < $val(nn) } {incr i} {
 $ns initial_node_pos $node_($i) 40
 }
 
 $ns at 1.1 "$node_(0) setdest 310.0 10.0 20.0"
-$ns at 1.1 "$node_(1) setdest 310.0 10.0 20.0"
+$ns at 1.1 "$node_(1) setdest 10.0 310.0 20.0"
 
 set tcp0 [new Agent/TCP]
 set sink0 [new Agent/TCPSink]
-$ns attach-agent $n0 $tcp0
-$ns attach-agent $n1 $sink0
-$ns connect $tcp0 $sink0
+$ns attach-agent $node_(0) $tcp0
+$ns attach-agent $node_(1) $sink0
 set ftp0 [new Application/FTP]
 $ftp0 attach-agent $tcp0
 
 $ns at 1.0 "$ftp0 start"
 $ns at 18.0 "$ftp0 stop"
 
-for {set i 0} {$i < $val(nn)} {incr i} {
-$ns at $val(stop) "$ns_($i) reset";
+for {set i 0} {$i < $val(nn) } {incr i} {
+$ns at $val(stop) "$node_($i) reset";
 }
-$ns at $val(stop) "puts \"NS EXITING...\"$ns halt";
+$ns at $val(stop) "puts \"Ns exiting...\";$ns halt";
 
-puts "Starting Simulation..."
+puts "starting simulation..."
 
 $ns run
