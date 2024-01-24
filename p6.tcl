2c624cc731c0bcc509df9d44c119d6374eef2ff3
Tejas M <97739480+playgms@users.noreply.github.com>
2024-01-24 22:10:11 +0530

    Update p6.tcl

--- p6.tcl
+++ p6.tcl
@@ -1,70 +1,44 @@
-set val(chan) Channel/WirelessChannel
-set val(prop) Propagation/TwoRayGround
-set val(netif) Phy/WirelessPhy
-set val(mac) Mac/802_11
-set val(ifq) Queue/DropTail/PriQueue
-set val(ll) LL
-set val(ant) Antenna/OmniAntenna
-set val(x) 500
-set val(y) 500
-set val(ifqlen) 50
-set val(nn) 2
-set val(stop) 20.0
-set val(rp) DSDV
+import numpy as np
 
-set ns [new Simulator]
-set tf [open ex6.tr w]
-set nf [open ex6.nam w]
-$ns trace-all $tf
-$ns namtrace-all-wireless $nf $val(x) $val(y)
+class Perceptron:
+    def __init__(self, input_size, learning_rate=0.1, threshold=0.0, max_iterations=1000):
+        self.weights = np.random.rand(input_size)
+        self.threshold = threshold
+        self.learning_rate = learning_rate
+        self.max_iterations = max_iterations
 
-set prop [new $val(prop)]
-set topo [new Topography]
-$topo load_flatgrid $val(x) $val(y)
+    def activate(self, net_input):
+        return 1 if net_input >= self.threshold else 0
 
-create-god $val(nn)
+    def train(self, input_data, labels):
+        iteration = 0
+        while iteration < self.max_iterations:
+            converged = True
+            for i in range(len(input_data)):
+                input_vector = np.array(input_data[i])
+                label = labels[i]
+                net_input = np.dot(input_vector, self.weights)
+                predicted_output = self.activate(net_input)
+                error = label - predicted_output
+                if error != 0:
+                    converged = False
+                    self.weights += self.learning_rate * error * input_vector
+            if converged:
+                break
+            iteration += 1
+        return iteration
 
-$ns node-config -adhocRouting $val(rp) \
-                -channelType $val(chan) \
-                -propType $val(prop) \
-                -phyType $val(netif) \
-                -macType $val(mac) \
-                -ifqType $val(ifq) \
-                -llType $val(ll) \
-                -antType $val(ant) \
-                -ifqLen $val(ifqlen) \
-                -topoInstance $topo \
-                -agentTrace ON \
-                -routeTrace ON \
-                -macTarce ON
+# Example usage
+input_data = [
+    [0, 0],
+    [0, 1],
+    [1, 0],
+    [1, 1]
+]
+labels = [0, 0, 0, 1]
 
-for {set i 0} {$i < $val(nn) } {incr i} {
-set node_($i) [$ns node]
-$node_($i) random-motion 0
-}
+perceptron = Perceptron(input_size=2)
+iterations = perceptron.train(input_data, labels)
 
-for {set i 0} {$i < $val(nn) } {incr i} {
-$ns initial_node_pos $node_($i) 40
-}
-
-$ns at 1.1 "$node_(0) setdest 310.0 10.0 20.0"
-$ns at 1.1 "$node_(1) setdest 10.0 310.0 20.0"
-
-set tcp0 [new Agent/TCP]
-set sink0 [new Agent/TCPSink]
-$ns attach-agent $node_(0) $tcp0
-$ns attach-agent $node_(1) $sink0
-set ftp0 [new Application/FTP]
-$ftp0 attach-agent $tcp0
-
-$ns at 1.0 "$ftp0 start"
-$ns at 18.0 "$ftp0 stop"
-
-for {set i 0} {$i < $val(nn) } {incr i} {
-$ns at $val(stop) "$node_($i) reset";
-}
-$ns at $val(stop) "puts \"Ns exiting...\";$ns halt";
-
-puts "starting simulation..."
-
-$ns run
+print("Converged in {} iterations".format(iterations))
+print("Final weights:", perceptron.weights)
